edf8400e3be9840fb220e7b095dd59765686eff0
ci-build <inanna+ci-build@recursion.wtf>
2020-08-25 00:25:14 +0000

    [skip ci] documentation update

--- unicase/sidebar-items.js
+++ unicase/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["UniCase","Case Insensitive wrapper of strings."]]});
+initSidebarItems({"fn":[["eq","Compare two string-like types for case-less equality, using unicode folding."],["eq_ascii","Compare two string-like types for case-less equality, ignoring ASCII case."]],"struct":[["Ascii","Case Insensitive wrapper of Ascii strings."],["UniCase","Case Insensitive wrapper of strings."]]});
